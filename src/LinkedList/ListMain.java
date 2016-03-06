3c92debe0b5c18b4ae1d11ecd658da193a3da65f
mybirth0407 <mybirth0407@gmail.com>
2016-03-07 00:51:04 +0900

    Single double list, ArrayList Stack, Array Queue

--- src/LinkedList/ListMain.java
+++ src/LinkedList/ListMain.java
@@ -1,4 +1,36 @@
 package LinkedList;
 
 public class ListMain {
+    public static void main(String args[]) {
+//        SingleLinkedList<Integer> sList = new SingleLinkedList<Integer>();
+//        sList.addLast(3);
+//        sList.addLast(4);
+//        sList.addLeft(4, 2);
+//        sList.addLast(5);
+//        sList.addRight(5, 7);
+//        sList.addLast(6);
+//        sList.printAllNode();
+//        sList.remove(7);
+//        sList.addLeft(3, 1);
+//        sList.addRight(6, 9);
+//        sList.remove(1);
+//        sList.remove(9);
+//        System.out.println("");
+//        sList.printAllNode();
+
+        DoubleLinkedList<Integer> dList = new DoubleLinkedList<Integer>();
+        dList.addLast(3);
+        dList.addLast(5);
+        dList.addLast(9);
+        dList.printAllNode();
+        System.out.println("");
+        dList.addLeft(3, 2);
+        dList.addRight(9, 10);
+        dList.addRight(5, 7);
+        dList.remove(3);
+        dList.addLeft(2, 1);
+        dList.remove(1);
+        dList.remove(10);
+        dList.printAllNode();
+    }
 }
--- src/test.java
+++ src/test.java
@@ -1,36 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class test {
     public static void main(String args[]) {
-        int arr[] = {2, 5, 3, 4, 3, 9, 3, 2, 5, 4, 1, 3};
-        int gap = (arr.length / 3) + 1;
-
-        ArrayList<Integer> arrayList[] = new ArrayList[gap];
-        for (int i = 0; i < arrayList.length; i++) {
-            arrayList[i] = new ArrayList<Integer>();
+        int cnt = 0;
+        int idx = 2;
+        while (cnt ++ < idx) {
+            System.out.println(cnt);
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            int j = i % gap;
-            arrayList[j].add(arr[i]);
-        }
-
-        for (int i = 0; i < gap; i++) {
-            Collections.sort(arrayList[i]);
-        }
-
-
-        for (int i = 0; i < gap; i++) {
-            int gapTmp = 0;
-            for (int j = 0; j < arrayList[i].size(); j++) {
-                arr[i + gapTmp] = arrayList[i].get(j);
-                gapTmp += gap;
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
     }
 }
